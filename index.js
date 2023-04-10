23f2ef6344fbfe9d097f2b63e24f83bef5e8a2fd
MdTarikulIslam <nayon1820@gmail.com>
2023-04-10 16:00:50 +0600

    news added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/category.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("News API running");
@@ -13,6 +14,26 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
